f5d16fb58f7eefcb6cca928f6d7dcec039a8b7bc
3dSpace116 <mirskam@gmail.com>
2024-11-01 03:09:43 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        String word = "Po top";
+
+        String cleaned = new String(word).replaceAll("\\s","").toLowerCase().toString();
+
+        String reversed = new StringBuilder(cleaned).reverse().toString();
+
+        if(cleaned.equals(reversed)){System.out.println("Это палиндром");}
+        else {System.out.println("Это не палиндром");}
+
     }
 }
